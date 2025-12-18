839fe7ddb75a453d9172a9b49e0a723923894caa
aarti <aarti.zinzuvadiya@appguruz.com>
2025-12-19 01:32:59 +0530

    code : initial user & cart Data  changes

--- src/Services/apiService.js
+++ src/Services/apiService.js
@@ -5,18 +5,3 @@ export const fetchProductsFromAPI = async () => {
   const response = await axios.get(PRODUCT_URL);
   return response.data.products;
 };
-
-const CART_URL = "https://dummyjson.com/carts?limit=50&skip=0";
-
-export const fetchCartsFromAPI = async () => {
-  const response = await axios.get(CART_URL);
-  return response.data.carts;
-};
-
-const USER_URL =
-  "https://dummyjson.com/users?limit=5&select=firstName,lastName,age,phone,email,password";
-
-export const fetchUsersFromAPI = async () => {
-  const response = await axios.get(USER_URL);
-  return response.data.users;
-};
